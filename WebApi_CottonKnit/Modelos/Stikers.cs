14a7bab6f51ad9acc6a170983e35b1be62391208
MAYCOL BENJAMIN HUAMANI FERNANDEZ <maycolhuamani12@gmail.com>
2020-12-01 14:52:39 -0500

    ac

---
+++ WebApi_CottonKnit/Modelos/Stikers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi_CottonKnit.Modelos
+{
+    public class Stikers
+    {
+        public string ordem_producao { get; set; }
+        public string ordem_confeccao { get; set; }
+    }
+}
---
+++ WebApi_CottonKnit/Repositorios/Interfaces/ILectStikersRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi_CottonKnit.Repositorios.Interfaces
+{
+    public interface ILectStikersRepository<T>
+    {
+        Task<object> GetAll(T obj);
+    }
+}
