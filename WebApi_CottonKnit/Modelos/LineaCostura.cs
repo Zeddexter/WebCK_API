ce0dd9161899eefd0c3311f850b1a0d95c5a95bc
jtorres <jtorres@SISTEMAS11>
2020-12-04 12:11:28 -0500

    Se agregó modelo LineaCostura

---
+++ WebApi_CottonKnit/Modelos/LineaCostura.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi_CottonKnit.Modelos
+{
+    public class LineaCostura
+    {
+        public int OP { get; set; }
+
+    }
+}
